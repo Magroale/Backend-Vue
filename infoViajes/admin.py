7fe97a74daf984c932e0c562ddb1d04ad321886b
Magroale <mario.alejo97@hotmail.com>
2023-11-15 20:31:59 -0500

    archivos para subir a git

--- infoViajes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from infoViajes.models import Viaje
-from infoViajes.models import Comentario
-# Register your models here.
-
-admin.site.register(Viaje)
-admin.site.register(Comentario)
