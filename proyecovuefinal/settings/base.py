523f39c019e77f98c427751230ce46be6b88d96f
Magroale <mario.alejo97@hotmail.com>
2023-11-15 21:54:00 -0500

    cambios a cors

--- proyecovuefinal/settings/base.py
+++ proyecovuefinal/settings/base.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
-    'http://127.0.0.1:8080',
+    'http://127.0.0.1:9000',
     'https://proyecto-vue-travel.vercel.app',
 ]
 
